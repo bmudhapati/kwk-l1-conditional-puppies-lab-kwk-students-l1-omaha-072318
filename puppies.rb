242589ddb8ba22650bb30ee8f86b5c708e5afe37
bmudhapati <bmudhapati957@millardps.org>
2018-07-25 16:25:25 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -42,7 +42,7 @@ end
 def both_animals (number_of_puppies, number_of_cats)
   if number_of_puppies || number_of_cats = 0
     "Good job!"
-  elsif number_of_puppies > 1 && number_of_cats > 1
+  elsif number_of_puppies >= 1 && number_of_cats >= 1
     "Get back to your animal goals!"
 end
 end
